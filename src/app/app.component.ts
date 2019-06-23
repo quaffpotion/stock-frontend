65d2629c1ad3d00f26821e6ce3fc7e3cb306afe8
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-06-23 14:23:18 -0500

    load local json file using require

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+var json = require('../../data.json')
+console.log(json)
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
