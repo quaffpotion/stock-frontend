c00ff74121b7e9721f6988865b385ec182351817
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-08-04 19:16:40 -0500

    adds wrapping and prevents default input box behavior that puts user cursor at the beginning when they would be using the down arrow to select

--- src/app/search-container/search-container.component.ts
+++ src/app/search-container/search-container.component.ts
@@ -5,48 +5,50 @@ import {
   ViewChild,
   ElementRef,
   AfterViewInit
-} from '@angular/core';
-import { Stock } from '../stock.model';
-import { Observable, of, Subject } from 'rxjs';
+} from "@angular/core";
+import { Stock } from "../stock.model";
+import { Observable, of, Subject } from "rxjs";
 import {
   tap,
   switchMap,
   debounceTime,
   distinctUntilChanged
-} from 'rxjs/operators';
+} from "rxjs/operators";
 
 @Component({
-  selector: 'app-search-container',
-  templateUrl: './search-container.component.html',
-  styleUrls: ['./search-container.component.css']
+  selector: "app-search-container",
+  templateUrl: "./search-container.component.html",
+  styleUrls: ["./search-container.component.css"]
 })
 export class SearchContainerComponent implements OnInit {
   @Input() mockdata: Stock[];
   searchTerms: Subject<string> = new Subject();
   stocks$: Observable<Stock[]>;
   stocks: Stock[] = [
-    new Stock('WRN', 'Warren', 67.11),
-    new Stock('BDN', 'Biden', 87.17),
-    new Stock('BRNI', 'Bernie', 45.12)
+    new Stock("WRN", "Warren", 67.11),
+    new Stock("BDN", "Biden", 87.17),
+    new Stock("BRNI", "Bernie", 45.12)
   ];
   hidden = true;
   selected: number;
+  listlength: number;
 
   constructor() {}
 
   pushSearchTerm(term: string) {
-    console.log('Pushing: ' + term);
+    console.log("Pushing: " + term);
     this.searchTerms.next(term);
   }
 
   searchStocks(term: string): Observable<Stock[]> {
     this.selected = 0;
     const result =
-      term == ''
+      term == ""
         ? []
         : this.mockdata.filter(
             item => item.name.includes(term) || item.symbol.includes(term)
           );
+    this.listlength = result.length;
     return of(result);
   }
 
@@ -55,7 +57,7 @@ export class SearchContainerComponent implements OnInit {
       debounceTime(500),
       distinctUntilChanged(),
       switchMap((term: string) => this.searchStocks(term)),
-      tap(searchResult => console.log('stocks found: ', searchResult))
+      tap(searchResult => console.log("stocks found: ", searchResult))
     );
   }
 
@@ -67,9 +69,14 @@ export class SearchContainerComponent implements OnInit {
     this.hidden = true;
   }
   handleDown() {
-    this.selected++;
+    this.selected = this.customMod(this.selected + 1, this.listlength);
   }
-  handleUp() {
-    this.selected--;
+  handleUp(e) {
+    e.preventDefault(); //default behavior puts cursor at beginning of input box
+    this.selected = this.customMod(this.selected - 1, this.listlength);
+  }
+
+  customMod(x, n) {
+    return ((x % n) + n) % n;
   }
 }
