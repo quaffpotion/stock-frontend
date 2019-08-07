1c6f765e931579863578ec02fb4ddbb7910839b8
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-08-06 23:31:13 -0500

    testing highlighter, need to deal with sanitization

--- src/app/highlight.pipe.ts
+++ src/app/highlight.pipe.ts
@@ -1,10 +1,101 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { PipeTransform, Pipe } from '@angular/core';
 
-@Pipe({
-  name: "highlight"
-})
+// @Pipe({ name: 'highlight' })
+// export class HighlightPipe implements PipeTransform {
+//   transform(text: string, search): string {
+//     return search
+//       ? text.replace(
+//           new RegExp(search, 'i'),
+//           `<span style="color: red;">${search}</span>`
+//         )
+//       : text;
+//   }
+// }
+
+@Pipe({ name: 'highlight' })
 export class HighlightPipe implements PipeTransform {
-  transform(value: string, args?: string): string {
-    return value.includes(args) ? value + "yes" : value + "no";
+  transform(text: string, search): string {
+    return search
+      ? text.replace(new RegExp(search, 'i'), `--${search}--`)
+      : text;
   }
 }
+
+// @Pipe({ name: 'highlight' })
+// export class HighlightPipe implements PipeTransform {
+//   transform(text: string, [search]): string {
+//     let s = 'CatDogCAT';
+//     let re = new RegExp('cat', 'gi');
+//     let matches = [...s.matchAll(re)].reverse();
+//     let el = document.createElement('div');
+//     el.style.display = 'inline';
+//     let splits = [...s.split(re)];
+//     splits.forEach(x => {
+//       if (x === '') {
+//         let y = document.createElement('span');
+//         y.textContent = matches.pop();
+//         y.style.color = 'green';
+//         y.style.fontWeight = 'bold';
+//         el.appendChild(y);
+//       } else {
+//         let z = document.createElement('span');
+//         z.textContent = x;
+//         el.appendChild(z);
+//       }
+//     });
+//     return `<span style="color: green; font - weight: bold; ">Cat</span><span>Dog</span><span style="color: green; font - weight: bold;">CAT</span>`;
+//   }
+// }
+
+/** Usage:
+ * <input type="text" [(ngModel)]="filter">
+ * <div [innerHTML]="myAwesomeText  | highlight : filter"></div>
+ *
+ */
+
+/*
+{
+    let term = 'cat';
+    //let s = "catdogcatcatcat"; //can't have this
+    let s = "CatDogCAT"
+    let re = new RegExp(term, 'gi');
+    let matches = [...s.matchAll(re)].reverse();
+    let el = document.createElement("div");
+    el.style.display='inline';
+    let splits = [...s.split(re)];
+    splits.forEach(x => {if (x === '') {let y = document.createElement("span"); y.textContent = matches.pop(); y.style.color='green'; y.style.fontWeight='bold'; el.appendChild(y)} else {let z = document.createElement("span"); z.textContent = x; el.appendChild(z);}})
+    console.log(el);
+    document.write(el.innerHTML)
+}
+ */
+
+/*
+
+
+import {PipeTransform, Pipe} from 'angular2/core';
+
+
+
+@Pipe({ name: 'highlight' })
+
+export class HighLightPipe implements PipeTransform {
+
+  transform(text: string, [search]): string {
+
+    return search ? text.replace(new RegExp(search, 'i'), `<span class="highlight">${search}</span>`) : text;
+
+  }
+
+}
+
+
+
+/** Usage:
+
+* <input type="text" [(ngModel)]="filter">
+
+* <div [innerHTML]="myAwesomeText  | highlight : filter"></div>
+
+*
+
+*/
