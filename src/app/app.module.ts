f5f31ea01c031a80c667dc843cc620975bb69cca
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-08-08 08:39:28 -0500

    removed old imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//Material Imports
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-//import { NoopAnimationsModule } from '@angular/platform-browser/animations'; //disable animations
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
 import { SearchContainerComponent } from './search-container/search-container.component';
 import { HighlightPipe } from './highlight.pipe';
@@ -20,12 +14,7 @@ import { SanitizeHtmlPipe } from './sanitize-html.pipe';
     SanitizeHtmlPipe
   ],
   //note, import order matters - must import after BrowserModule
-  imports: [
-    BrowserModule,
-    MatListModule,
-    BrowserAnimationsModule,
-    MatIconModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
