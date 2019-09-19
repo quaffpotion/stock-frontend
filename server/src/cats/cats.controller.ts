870b0abb9a37b38829568b422223acc5f08d840b
Brian Chirgwin <bchirgwin42@gmail.com>
2019-09-18 23:12:47 -0500

    move frontend to frontend and add server project

---
+++ server/src/cats/cats.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Body, Param, Post, Redirect } from '@nestjs/common';
+import { Request } from 'express';
+import {CreateClassDto} from './create-cats.dto'
+
+
+@Controller('cats')
+export class CatsController {
+  @Post()
+  async create(@Body() createCatDto: CreateClassDto) {
+    return createCatDto;
+  }
+
+  @Get()
+  findall(): string {
+    return 'This action returns all cats!!!';
+  }
+
+  @Get(':name')
+  hello(@Param('name') name): string {
+    return `meow, ${name}`;
+  }
+
+}
---
+++ server/src/cats/create-cats.dto.ts
@@ -0,0 +1,5 @@
+export class CreateClassDto {
+  name: string;
+  color: string;
+  age: number;
+}
