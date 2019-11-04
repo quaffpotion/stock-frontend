ff163553ffb917813b497a9acf56820ed61a0b73
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-11-03 23:31:18 -0600

    basic candlestick chart

--- frontend/src/app/candlestick-chart/candlestick-chart.component.ts
+++ frontend/src/app/candlestick-chart/candlestick-chart.component.ts
@@ -6,9 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./candlestick-chart.component.css']
 })
 export class CandlestickChartComponent {
+  min = Math.min;
+  max = Math.max;
+  abs = Math.abs;
   data: number[];
+  stocks: {
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+  }[];
   constructor() {
     this.data = [...new Array(1000)].map(() => Math.random() * 100);
-    console.log(this.data);
+    this.stocks = [...new Array(1000)].map(() => ({
+      open: Math.random() * 100,
+      high: 50 + Math.random() * 50,
+      low: 50 - Math.random() * 50,
+      close: Math.random() * 100
+    }));
+  }
+
+  minmax(stock) {
+    return (
+      Math.max(stock.open, stock.close, stock.low, stock.high) -
+      Math.min(stock.open, stock.close, stock.low, stock.high)
+    );
+  }
+
+  upday(stock) {
+    if (stock.close >= stock.open) {
+      return 'green';
+    } else {
+      return 'red';
+    }
   }
 }
