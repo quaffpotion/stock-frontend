4919bbed2864ff36361fd678a090f6adee4c7139
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-11-05 15:25:44 -0600

    removed repo for iex-api, will make own

--- frontend/src/data-iex.service.ts
+++ frontend/src/data-iex.service.ts
@@ -1,20 +1,13 @@
 import { Injectable } from '@angular/core';
-import { IEXClient } from 'iex-api';
 import { HttpClient } from '@angular/common/http';
-import * as _fetch from 'isomorphic-fetch';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataIexService {
-  iex: IEXClient;
-
-  constructor(private http: HttpClient) {
-    this.iex = new IEXClient(_fetch);
-  }
+  constructor(private http: HttpClient) {}
 
   getCompanyBySymbol(symbol: string) {
     this.http.get('/api/tops').subscribe(j => console.log(j));
-    // this.iex.stockCompany('AAPL').then(company => console.log(company)); //Returns forbidden, perhaps API is outdated?
   }
 }
