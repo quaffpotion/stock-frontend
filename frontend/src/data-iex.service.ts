3423e97d12884debfe84d41ea881592c18cba2dd
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-12-01 10:39:53 -0600

    API requests to IEX work

--- frontend/src/data-iex.service.ts
+++ frontend/src/data-iex.service.ts
@@ -1,13 +1,15 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataIexService {
   constructor(private http: HttpClient) {}
 
   getCompanyBySymbol(symbol: string) {
-    this.http.get('/api/tops').subscribe(j => console.log(j));
+    this.http
+      .get("https://api.iextrading.com/1.0/tops/last")
+      .subscribe(j => console.log(j));
   }
 }
